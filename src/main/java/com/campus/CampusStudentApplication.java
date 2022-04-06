a6312b3298873dd835d41cbc4a46868e4d686009
winter-qiuku <butter168@126.com>
2022-04-06 18:08:21 +0800

    第一次

---
+++ src/main/java/com/campus/CampusStudentApplication.java
@@ -0,0 +1,13 @@
+package com.campus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusStudentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/campus/CampusStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusStudentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
